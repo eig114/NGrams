2600fa269f10ae04f687b9f4b5a4e5abdd6602d7
EiG <eig114@gmail.com>
2014-06-25 12:21:46 +0400

    [+] Получение отностельных расстояний Матсуиты для нескольких авторов

--- DistanceCalculation/MatsuitaDistance.cs
+++ DistanceCalculation/MatsuitaDistance.cs
@@ -1,18 +1,17 @@
 using System;
-using System.IO;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using NGrams.Profiles;
-using NGrams.Extensions;
+
 namespace NGrams.DistanceCalculation
 {
     /// <summary>
     /// d = sqrt(sum((sqrt(xi) - sqrt(yi))^2))
+    /// Может быть весьма медленным 
     /// </summary>
     public static class MatsuitaDistance
     {
-        public static double GetMatsuitaDistance<Criteria>(this IProfile<Criteria> profile1, IProfile<Criteria> profile2){
+        public static double GetMatsuitaDistance<TCriteria>(this IProfile<TCriteria> profile1, IProfile<TCriteria> profile2){
             var criteries = profile1.RawOccurencies.Keys.Union(profile2.RawOccurencies.Keys);
 
             double sum=0;
@@ -25,6 +24,27 @@ namespace NGrams.DistanceCalculation
 
             return Math.Sqrt(sum);
         }
+
+		public static IDictionary<IProfile<TCriteria>, double> GetMatsuitaDistances<TCriteria>(
+			this IProfile<TCriteria> p1,
+			IEnumerable<IProfile<TCriteria>> other)
+		{
+			var result = new Dictionary<IProfile<TCriteria>, double>();
+			double sum = 0;
+			foreach (var p2 in other)
+			{
+				double distance = p1.GetMatsuitaDistance(p2);
+				sum += distance;
+				result.Add(p2, distance);
+			}
+
+			double sum2 = result.Sum(x => sum - x.Value);
+			result = result
+				.Select(x => new KeyValuePair<IProfile<TCriteria>, double>(x.Key, (sum - x.Value) / sum2))
+				.ToDictionary(x => x.Key, y => y.Value);
+
+			return result;
+		}
     }
 }
 
